bc7389c86454c6a63f46555bebc40b7073cabad9
ericremoroza <eric.remoroza@outlook.com>
2018-10-28 14:53:32 -0400

    fixed still and animated functions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,75 +1,69 @@
-$(document).ready(function () {
-    var kPop = ["2NE1", "BTS", "Girls' Generation", "Psy", "Hello Bitches", "Mr. Simple", "SHINee", "Super Junior"];
+$(function () {
+    createButtons(kPop, "searchButton", "#buttons-view");
+    console.log("KPOP ROCKS!");
+});
 
-    //adds buttons based on user-input
-    function createButtons() {
-        // deletes added content to prevent repeat buttons
-        $("#buttons-view").empty();
-        for (var i = 0; i < kPop.length; i++) {
-            var a = $("<button>");
-            a.addClass("artistOrSong");
-            a.attr("data-name", kPop[i]);
-            a.text(kPop[i]);
-            $("#gifs-view").append(a);
-            console.log(kPop[i]);
-        }
-    };
+var kPop = ["2NE1", "BTS", "Girls' Generation", "Psy", "Hello Bitches", "Mr. Simple", "SHINee", "Super Junior"];
 
-    $("#kpop-button").on("click", function (event) {
-        event.preventDefault();
-        var term = $("#kpop-input").val().trim();
-        kPop.push(term);
-        createButtons();
+//adds buttons based on user-input
+function createButtons(kPop, artistOrSong, addToArea) {
+    // deletes added content to prevent repeat buttons
+    $(addToArea).empty();
+    for (var i = 0; i < kPop.length; i++) {
+        var a = $("<button>");
+        a.addClass(artistOrSong);
+        a.attr("data-type", kPop[i]);
+        a.text(kPop[i]);
+        $(addToArea).append(a);
 
-    });
+    }
+};
 
-    $(".gif").on("click", function () {
-        var state = $(this).attr("data-state");
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-    });
-    
-    function displayGifs() {
-        var term = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=8uW2w5RcnSMn8ltX5jYkG3Rr2i5I50Qw&limit=10";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(queryURL);
-            $("#gifs-view").empty();
-            for (var i = 0; i < response.data.length; i++) {
-                var gifImg = $("<img src='" + response.data[i].images.fixed_height_still.url + "'>");
-                gifImg.addClass('gif');
-
+$(document).on("click", ".searchButton", function() {
+    var term = $(this).data("type");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=8uW2w5RcnSMn8ltX5jYkG3Rr2i5I50Qw&limit=5";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).done(function(response) {
+            for (var i = 0; i < response.data.length;i++) {
                 var gifDiv = $("<div class='gif-div'>");
-                gifDiv.html("<p>Rating: " + response.data[i].rating + "</p>");
-
-                var imgDiv = $("<div class='play'>");
-                imgDiv.attr("data-state", "still");
-                imgDiv.attr("data-name", term);
-                imgDiv.attr("data-still", response.data[i].images.fixed_height_still.url);
-                imgDiv.attr("data-animate", response.data[i].images.fixed_height.url);
-
-                $(imgDiv).append(gifImg);
-                $(gifDiv).append(imgDiv);
+                //keep it clean
+                var rating = response.data[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                //animated GIF
+                var animated = response.data[i].images.fixed_height.url;
+                //still image of GIF
+                var still = response.data[i].images.fixed_height_still.url;
+                var gifImg = $("<img>");
+                gifImg.attr("src", still);
+                gifImg.attr("data-still", still);
+                gifImg.attr("data-animated", animated);
+                gifImg.attr("data-state", "still");
+                gifImg.addClass("searchImage");
+                gifDiv.append(p);
+                gifDiv.append(gifImg);
                 $("#gifs-view").append(gifDiv);
             }
+        })
+});
 
+//animate or still gif
+$(document).on("click", ".searchImage", function() {
+    var state = $(this).attr("data-state");
+    if(state == "still") {
+        $(this).attr("src", $(this).data("animated"));
+        $(this).attr("data-state", "animated");
+    } else {
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+    }
+});
 
-
-        });
-    };
-
-
-
-
-    createButtons();
-    displayGifs();
+//user-added new buttons
+$("#newButton").on("click", function() {
+    var newTerm = $("input").eq(0).val();
+    kPop.push(newTerm);
+    createButtons(kPop, "searchButton", "#buttons-view");
+    return false;
 });
